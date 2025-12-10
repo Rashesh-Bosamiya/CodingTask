bf7578face1467e5e2694f221cd78088735c8469
Rashesh Bosamiya <rashesh.bosamiya@accenture.com>
2025-12-10 11:47:27 +0530

    Initial Commit

---
+++ CodingTask/CodingTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingTaskApp.swift
+//  CodingTask
+//
+//  Created by rashesh.bosamiya on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CodingTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
